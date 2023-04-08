ee6aa676494b2f9659d5cd0cb1049c73a5bb31f1
Andrey Dmitriev <admit8490@gmail.com>
2023-04-08 21:53:53 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+const App = () => {  
+  return (
+    <div className="App">      
+      <h1>this is app</h1>
+    </div>
+  )
+}
+
+export default App
