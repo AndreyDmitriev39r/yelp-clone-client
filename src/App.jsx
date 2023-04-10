5f5b51c0a6bd753444391899be4ca211da685872
Andrey Dmitriev <admit8490@gmail.com>
2023-04-10 19:44:24 +0200

    client side routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import Update from './routes/Update';
+
 const App = () => {  
   return (
-    <div className="App">      
-      <h1>this is app</h1>
+    <div>      
+      <Router>
+        <Routes>
+          <Route exact path='/' Component={Home}/>
+          <Route exact path='/restaurants/:id' Component={Detail}/>
+          <Route exact path='/restaurants/:id/update' Component={Update}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
